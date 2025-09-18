53b402f00195780fdaa4572c662b56b99fe11256
Thomas Steimann <thomas@steimann.ch>
2025-09-18 17:56:39 +0200

    updated user list in app component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,17 @@
 import { Component, computed, Input, input, Output, EventEmitter, output } from '@angular/core';
 
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 //const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
@@ -10,18 +22,20 @@ import { Component, computed, Input, input, Output, EventEmitter, output } from 
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
   @Output() select = new EventEmitter<string>();
   //select = output<string>();
   
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
